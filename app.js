eb78190e991247eb05e6c35ff62d565bc3ca063e
shahriartamim2 <abdnoman1616@gmail.com>
2024-07-08 15:06:46 +0600

    MVC : Added database connection

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import bodyParser from "body-parser";
 import express from "express";
+import connectDb from "./models/products.model.js";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+
+connectDb();
 app.get('/',(req,res)=>{
     res.send("Alhamdulillah chole")
 })
--- models/products.model.js
+++ models/products.model.js
@@ -1,2 +1,14 @@
 import mongoose from "mongoose";
+import 'dotenv/config';
+
+const connectDb = async ()=>{
+    try {
+       await  mongoose.connect(process.env.MONGODB_URI);
+        console.log("Successfully connected to Db");
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default connectDb;
 
