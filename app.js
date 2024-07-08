8255172feae8cc5c3a8eec64f0c28a866b5f49b5
shahriartamim2 <abdnoman1616@gmail.com>
2024-07-08 14:47:07 +0600

    MVC structure: updated bodyParser

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("Alhamdulillah chole")
 })
